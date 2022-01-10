39f0644bed50a56bc3eddab58651343dd20ae26a
necmigunduz <necmigunduz@gmail.com>
2022-01-10 12:40:48 +0300

    Add useState

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import { Searchbar } from 'react-native-paper';
 
 function App() {
+  const [input, setInput] = useState("");
   return (
     <View style={styles.container}>
       <Searchbar 
         placeholder="Search"
-        onChangeText={()=>{}}
+        onChangeText={(text)=>{
+          setInput(text);
+        }}
         value=""
       />
     </View>
